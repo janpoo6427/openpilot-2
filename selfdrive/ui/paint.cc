bda820731b92e670bdee7adc71f297f6c7249320
neokii <neokii@gmail.com>
2020-06-20 14:36:27 +0900

    fix curvature to degree

--- selfdrive/ui/paint.cc
+++ selfdrive/ui/paint.cc
@@ -671,7 +671,7 @@ static void bb_ui_draw_L_PID(UIState *s)
     ui_draw_text(s->vg, text_x, y, str, 25 * 2.5, COLOR_WHITE_ALPHA(200), s->font_sans_regular);
 
     y += height;
-    snprintf(str, sizeof(str), "CURV: %.2f", scene->curvature);
+    snprintf(str, sizeof(str), "CURV: %.1f°", scene->curvature);
     ui_draw_text(s->vg, text_x, y, str, 25 * 2.5, COLOR_WHITE_ALPHA(200), s->font_sans_regular);
 }
 

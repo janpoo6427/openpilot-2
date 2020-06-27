fc48449366d5b11f24f4f8461a8854abc7e18e15
neokii <neokii@gmail.com>
2020-06-27 17:42:51 +0900

    tuning

--- selfdrive/ui/ui.cc
+++ selfdrive/ui/ui.cc
@@ -450,7 +450,7 @@ void handle_message(UIState *s,  Message* msg) {
     scene.pid_d = l_pid.getD();
     scene.pid_output = l_pid.getOutput();
 
-    scene.pcurvature = data.getPCurvature();
+    scene.pCurvature = data.getPCurvature();
 
   } else if (which == cereal::Event::RADAR_STATE) {
     auto data = event.getRadarState();

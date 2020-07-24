f54157aa25c2843c7fdc458862cb1559e0b87bd7
neokii <neokii@gmail.com>
2020-07-24 17:52:03 +0900

    Fix blinker.

--- selfdrive/car/hyundai/carcontroller.py
+++ selfdrive/car/hyundai/carcontroller.py
@@ -98,10 +98,10 @@ class CarController():
 
     # Disable steering while turning blinker on and speed below 60 kph
     if CS.out.leftBlinker or CS.out.rightBlinker:
-      if self.car_fingerprint not in [CAR.KIA_OPTIMA, CAR.KIA_OPTIMA_H]:
-        self.turning_signal_timer = 100  # Disable for 1.0 Seconds after blinker turned off
-      elif CS.left_blinker_flash or CS.right_blinker_flash: # Optima has blinker flash signal only
-        self.turning_signal_timer = 100
+      self.turning_signal_timer = 100  # Disable for 1.0 Seconds after blinker turned off
+    elif CS.left_blinker_flash or CS.right_blinker_flash: # Optima has blinker flash signal only
+      self.turning_signal_timer = 100
+
     if self.turning_signal_timer and CS.out.vEgo < 60 * CV.KPH_TO_MS:
       lkas_active = 0
     if self.turning_signal_timer:

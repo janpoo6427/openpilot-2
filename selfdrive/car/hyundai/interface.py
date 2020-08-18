7648c037fc05d37eb11750d05fae811c184a52d2
neokii <neokii@gmail.com>
2020-08-18 22:10:02 +0900

    tuning

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -154,7 +154,7 @@ class CarInterface(CarInterfaceBase):
     ret.lateralTuning.init('lqr')
 
     ret.lateralTuning.lqr.scale = 1700.0
-    ret.lateralTuning.lqr.ki = 0.05
+    ret.lateralTuning.lqr.ki = 0.03
 
     ret.lateralTuning.lqr.a = [0., 1., -0.22619643, 1.21822268]
     ret.lateralTuning.lqr.b = [-1.92006585e-04, 3.95603032e-05]
@@ -165,11 +165,11 @@ class CarInterface(CarInterfaceBase):
 
     ret.steerRatio = 15.2
     ret.steerActuatorDelay = 0.4
-    ret.steerRateCost = 0.5
+    ret.steerRateCost = 0.55
     ret.steerLimitTimer = 1.3
 
-    ret.steerMaxBP = [0.]
-    ret.steerMaxV = [1.5]
+    ret.steerMaxBP = [30. * CV.KPH_TO_MS, 50. * CV.KPH_TO_MS]
+    ret.steerMaxV = [1., 1.5]
 
 
 

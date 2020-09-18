764f224ff405dcc44c87d8320dd0f65766f46919
wikarous <67613740+wikarous@users.noreply.github.com>
2020-09-18 12:28:45 +0900

    Update lane_planner.py
    차선가중치, 조로바이트

--- selfdrive/controls/lib/lane_planner.py
+++ selfdrive/controls/lib/lane_planner.py
@@ -4,6 +4,9 @@ from cereal import log
 
 CAMERA_OFFSET = 0.06  # m from center car to camera
 
+#zorrobyte
+def mean(numbers): 
+     return float(sum(numbers)) / max(len(numbers), 1)
 
 def compute_path_pinv(l=50):
   deg = 3
@@ -56,9 +59,13 @@ class LanePlanner():
     self.p_poly = [0., 0., 0., 0.]
     self.d_poly = [0., 0., 0., 0.]
 
-    self.lane_width_estimate = 3.7
-    self.lane_width_certainty = 1.0
-    self.lane_width = 3.7
+    #self.lane_width_estimate = 3.7
+    #self.lane_width_certainty = 1.0
+    #self.lane_width = 3.7
+    #zorro
+    self.lane_width = 2.85
+    self.readings = []
+    self.frame = 0
 
     self.l_prob = 0.
     self.r_prob = 0.
@@ -90,14 +97,21 @@ class LanePlanner():
     self.l_poly[3] += CAMERA_OFFSET
     self.r_poly[3] += CAMERA_OFFSET
 
-    # Find current lanewidth
-    self.lane_width_certainty += 0.05 * (self.l_prob * self.r_prob - self.lane_width_certainty)
-    current_lane_width = abs(self.l_poly[3] - self.r_poly[3])
-    self.lane_width_estimate += 0.005 * (current_lane_width - self.lane_width_estimate)
-    speed_lane_width = interp(v_ego, [0., 31.], [2.8, 3.5])
-    self.lane_width = self.lane_width_certainty * self.lane_width_estimate + \
-                      (1 - self.lane_width_certainty) * speed_lane_width
-
+    #zorrobyte code
+    if self.l_prob > 0.49 and self.r_prob > 0.49:
+      self.frame += 1
+      if self.frame % 20 == 0:
+        self.frame = 0
+        current_lane_width = sorted((2.5, abs(self.l_poly[3] - self.r_poly[3]), 3.5))[1]
+        max_samples = 30
+        self.readings.append(current_lane_width)
+        self.lane_width = mean(self.readings)
+        if len(self.readings) == max_samples:
+          self.readings.pop(0)
+    if abs(self.l_poly[3] - self.r_poly[3]) > self.lane_width:
+      self.r_prob = self.r_prob / interp(self.l_prob, [0, 1], [1, 3])
+    #end of zorrobyte
+    
     self.d_poly = calc_d_poly(self.l_poly, self.r_poly, self.p_poly, self.l_prob, self.r_prob, self.lane_width, v_ego)
 
   def update(self, v_ego, md):
